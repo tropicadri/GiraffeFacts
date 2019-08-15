61a0221892e70a1e3fa51def6810f8bedb9241c8
Adri <adrianajara@gmail.com>
2019-08-14 18:05:23 -0600

    adding drawer

--- app/src/main/java/com/example/giraffefacts/MainActivity.kt
+++ app/src/main/java/com/example/giraffefacts/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.giraffefacts
 
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.core.provider.FontRequest
 import androidx.core.text.HtmlCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.emoji.text.EmojiCompat
 import androidx.emoji.text.FontRequestEmojiCompatConfig
 import kotlin.random.Random
@@ -16,7 +21,9 @@ class MainActivity : AppCompatActivity() {
 
     val TAG = "EmojiCompatApplication"
     val EMOJI = "\ud83e\udd92"
-    lateinit var factTextView:TextView
+    lateinit var factTextView: TextView
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,19 +57,46 @@ class MainActivity : AppCompatActivity() {
 
         factTextView = findViewById(R.id.fact_text_view)
 
+        HtmlCompat.fromHtml(getString(R.string.fact_source), HtmlCompat.FROM_HTML_MODE_LEGACY);
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-         HtmlCompat.fromHtml(getString(R.string.fact_source), HtmlCompat.FROM_HTML_MODE_LEGACY);
+        drawer = findViewById(R.id.drawer_layout)
 
-
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
     }
 
     fun showFact(view: View) {
-        val nextFact = Random.nextInt(0,10)
+        val nextFact = Random.nextInt(0, 10)
         val facts = resources.getStringArray(R.array.facts)
         val fact = facts[nextFact];
-        factTextView.text=fact;
+        factTextView.text = fact;
+    }
 
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
 
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
